abd70b1f6721fda35f79f06c52785a5c8a5cdbf9
ryanbusby <busb87@gmail.com>
2021-04-18 19:54:28 -0600

    change name of project

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://ryanbusby@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://ryanbusby@localhost:5432/gigs'
